d8093f1cd8b805f01e064c590414151167808d39
Zeeson Manandhar <zeeson.manandhar11@gmail.com>
2025-12-04 07:34:42 +0545

    Added new file Git1

---
+++ git1.py
@@ -0,0 +1,9 @@
+# Branches => Branching allows multiple collaborators work on same project
+# PR => Pull request => A request to the owner of the github to merge your code in master branch
+# To create a branch  => git branch {branch_name}
+# git checkout -b {branch name}
+
+# To view the branches => git branch
+# to switch between the branches => git switch{branch name}
+
+# Changes Made
