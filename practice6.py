4212ec98f919d0a9a4e653558b729f266016664d
Zeeson Manandhar <zeeson.manandhar11@gmail.com>
2026-01-27 12:23:53 +0545

    updated

--- practice6.py
+++ practice6.py
@@ -1,7 +1,7 @@
 # Dictionaries ---- Practice Questions:
 
 
-# BASIC LEVEL (1–10)
+# BASIC LEVEL
 
 # Write a program to create a dictionary with 5 key–value pairs and print it.
 
@@ -83,49 +83,194 @@ new_dict = mountains.copy()
 print(new_dict)
 
 
-# INTERMEDIATE LEVEL (11–20)
+# INTERMEDIATE LEVEL
 
 # Write a program to add a new key–value pair to an existing dictionary.
 
+bikes = {
+    "model": "NS200",
+    "brand": "Bajaj",
+    "year": 2021,
+    "electric": False
+}
+
+bikes["colors"] = "silver,red & black"
+
+print(bikes)
+
 # Write a program to update the value of an existing key.
+
+bikes["year"] = 2022
+print(bikes)
 
 # Write a program to remove a key using del.
 
+del bikes["brand"]
+
+print(bikes)
+
 # Write a program to remove a key using pop() and print the removed value.
+
+rem_val = bikes.pop("electric")
+print(bikes)
+print(rem_val)
 
 # Write a program to clear all elements of a dictionary.
 
+bikes.clear()
+print(bikes)
+
 # Write a program to loop through all keys in a dictionary.
+
+players_scores = {
+    'Alice': 150,
+    'Bob': 200,
+    'Charlie': 120
+}
+
+for item in players_scores.keys():
+    print(item)
 
 # Write a program to loop through all values in a dictionary.
 
+for value in players_scores.values():
+    print(value)
+
 # Write a program to loop through all key–value pairs and print them.
+
+for key , value in players_scores.items():
+    print(f"key = {key}, value = {value}")
 
 # Write a program to merge two dictionaries into one.
 
+dict_1 = {
+    "Name": "Salman Khan",
+    "DOB": "1975-01-01",
+}
+
+dict_2 = {
+    "country": "India",
+    "Profession" : "Actor"
+}
+dict_1.update(dict_2)
+print(dict_1)
+
+# Alternative Way: 
+new_dict = {**dict_1,**dict_2}
+print(new_dict)
+
 # Write a program to create a dictionary from two lists (keys list and values list).
 
-# LOGIC-BASED (IMPORTANT) (21–30)
+list1 = ["Name", "Age", "Profession"]
+list2 = ["Zeeson", 22, "Student"]
 
+data = {}
+
+for i in range(len(list1)):
+    data[list1[i]] = list2[i]
+
+print(data)
+
+
+
+# LOGIC-BASED (IMPORTANT) 
 # Write a program to count the frequency of each character in a string.
 
 # Write a program to count the frequency of each word in a sentence.
 
 # Write a program to find the key with the maximum value in a dictionary.
 
+data = {
+    "a":5,
+    "b":15,
+    "c":6
+}
+val = list(data.values())
+max_value = max(val)
+print(max_value)
+
+l1 = []
+
+for key, value in data.items():
+    if value == max_value:
+        l1.append(key)
+
+print(f"The key with maximum value is {l1}")
+
+
 # Write a program to find the key with the minimum value in a dictionary.
+
+data1 = {
+    "a": 6,
+    "b": 7,
+    "c": 8
+}
+min_val = min(data1)
+print( f"The minimum value is {data1[min_val]} and its key is {[min_val]}")
+
 
 # Write a program to check if a dictionary is empty or not.
 
+student_scores = {
+    "Alice": 95,
+    "Bob": 88,
+    "Charlie": 92
+}
+
+if len(student_scores) == 0:
+    print("The Dictionary is empty")
+else:
+    print("Not Empty")
+
+
 # Write a program to create a dictionary of numbers (1–10) and their squares.
+
+num = {}
+
+for i in range(1,11):
+    num[i] = i**2
+print(num)
 
 # Write a program to create a dictionary of numbers and their cubes using dictionary comprehension.
 
+
+cube_num = {item : item**3 for item in range(1,11)}
+print(cube_num)
+
 # Write a program to remove all duplicate values from a dictionary.
+
+dict_1 = {"a": 10, "b": 20, "c": 10, "d": 30}
+rem_val = {}
+checked_val = set()
+
+for key ,value in dict_1.items():
+    if value not in checked_val:
+        rem_val[key] = value
+        checked_val.add(value)
+
+print(rem_val)
 
 # Write a program to sort a dictionary by its keys.
 
-# Write a program to sort a dictionary by its values.
+dict_1 = {
+    'a' : 1,
+    'c' : 3,
+    'd' : 4,
+    'b':2
+}
+
+dict_keys = dict_1.keys()
+sort_keys = sorted(dict_keys)
+print(sort_keys)
+
+new_dict = {
+}
+
+for item in sort_keys:
+    new_dict[item] = dict_1[item]
+
+print(new_dict)
+   
 
 # ADVANCED / REAL-WORLD (31–40)
 
