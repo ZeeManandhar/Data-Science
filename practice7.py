440c9486643dc770e5b5c3fa6da6a3fb14d4598d
Zeeson Manandhar <zeeson.manandhar11@gmail.com>
2026-01-30 15:07:42 +0545

    updated

--- practice7.py
+++ practice7.py
@@ -1,34 +1,119 @@
-# 1. BASIC LEVEL (Foundations) --- Questions 1–14
+# 1. BASIC LEVEL (Foundations) 
 
 # Write a program to check whether a number is positive.
 
+num = 5
+
+if num > 0:
+    print(f"{num} is Positive Number.")
+else:
+    print(f"{num} is Negative Number.")
+
+
 # Write a program to check whether a number is negative.
+
+num = -1
+
+if num < 0:
+    print("Negative Number")
+else:
+    print("Not a Negative Number")
 
 # Write a program to check whether a number is zero.
 
+num = 0
+
+if num == 0:
+    print("Zero")
+else:
+    print("Not Zero")
+
 # Write a program to check whether a number is greater than 100.
+
+num1 = int(input("Enter a Number: "))
+
+if num1 > 100:
+    print(True)
+else:
+    print(False)
+
 
 # Write a program to check whether a number is even.
 
+number = 7
+
+if number%2 == 0:
+    print("Number is Even")
+else:
+    print("Not an Even Number")
+
 # Write a program to check whether a number is odd.
 
+number = 8
+
+if number%2 != 0:
+    print("Odd Number!")
+else:
+    print("Not an Odd Number!")
+
 # Write a program to check whether a person is eligible to vote.
+
+age = int(input("Enter your Age Please: "))
+
+if age >= 18:
+    print("Eligible to Vote!")
+else:
+    print("Not Eligible!")
 
 # Write a program to check whether a temperature is above freezing point.
 
 # Write a program to check whether a number is positive or negative.
 
+num1 = int(input("Enter your number: "))
+
+if num1 > 0:
+    print("Positive Number!")
+else:
+    print("Negative Number!")
+
 # Write a program to check whether a student has passed or failed (pass mark = 40).
 
-# Write a program to check whether a number is even or odd.
+marks = int(input("Enter your marks please: "))
+
+if marks >= 40:
+    print("You are Passed!")
+else:
+    print("You Failed")
+
 
 # Write a program to compare two numbers and print the greater one.
 
+num1 = int(input("Enter The First Number: "))
+num2 = int(input("Enter The Second Number: "))
+
+if num1 > num2 :
+    print(num1)
+else:
+    print(num2)
+
 # Write a program to check whether a person is adult or minor.
+age = int(input("Enter your Age Please: "))
 
-# Write a program to check whether a year is leap year or not (simple logic).
+if age >= 18:
+    print("Adult")
+else:
+    print("Minor")
 
-# 2. INTERMEDIATE LEVEL (Control Flow & Decisions) — Questions 15–28
+# Write a program to check whether a year is leap year or not .
+
+year = int(input("Enter the year: "))
+
+if year % 4 == 0 and year % 100 != 0:
+    print("Leap Year!")
+else:
+    print("Not a Leap Year!")
+
+# 2. INTERMEDIATE LEVEL (Control Flow & Decisions) 
 
 # Write a program to validate a password.
 
