f1756dfa4af324e078d0c4f487d47abbd089dbfd
Zeeson Manandhar <zeeson.manandhar11@gmail.com>
2026-02-15 19:43:16 +0545

    updated

--- file_handling.py
+++ file_handling.py
@@ -82,3 +82,102 @@ f.close()
 # Since file didn’t exist ----> Python created it
 # Then we closed it
 
+# 3.) File Modes
+
+# When we open a file:
+
+# open("demo.txt", "mode") # This mode tells Python what we want to do with the file.
+
+# 3.1) "r" --- READ MODE
+
+f = open("demo1.txt", "r")
+print(f.read())  
+f.close()
+
+# Meaning:
+
+# Open file to read
+# File MUST exist
+# If file does not exist, it shows error.
+
+# Use when:
+# You only want to read content.
+
+
+# 3.2) "w" -- Write Mode
+
+f = open("demo.txt","w")
+f.write("Today is Sunday! Have a Good Day!")
+file.close()
+
+# Meaning:
+
+# Write into file.
+# If file does NOT exist, then it creates it.
+# If file exists, then it deletes old content and replaces it.
+
+# Key Important To Remember: "w" erases old content.
+
+# 3.3) "append" --- Append Mode
+
+f = open("demo.txt","a")
+f.write("Added New Text!")
+f.close()
+
+# Meaning:
+# Add new content at the end (must use .write() method, there is no append method.)
+# Does NOT delete old content
+# Creates file if not exist
+
+# 3.4) "x" --- Create Mode
+
+# f = open("demo2.txt", "x")
+# f.write("Hello! I am learning File Hnadling.")
+# f.close()
+
+# Meaning:
+
+# Create new file only (Method used: write())
+# If file already exists, then it throws an error 
+
+# Used when:
+# we want to ensure file does NOT already exist.
+
+# 3.5) "r+" ---- Read and Write On
+
+f = open("demo2.txt","r+")
+f.write("Hi")
+print(f.read())
+f.close()
+
+# Meaning:
+# Read and write
+# File must exist
+# Does NOT erase automatically
+
+# Key Note To Remember for ""r+": 
+# It will overwrite from the start.
+# It does NOT erase entire file like "w".
+# It only overwrites from current position.
+
+
+# 4.) Reading Files
+
+# 4.1) read() Method
+
+f = open("demo1.txt", "r")
+
+print("First read:")
+print(f.read())
+
+# It reads the entire file
+# Returns it as a string
+
+# After read(), the cursor goes to the end of file.
+# So if we try to read again:
+print("Second read:")
+print(f.read())
+
+# It will print nothing.
+# Because it already reached the end.
+
