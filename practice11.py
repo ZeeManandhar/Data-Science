2fc1d99d4450e2378128a67f1df82be409176e6a
Zeeson Manandhar <zeeson.manandhar11@gmail.com>
2026-03-23 20:02:48 +0545

    Updated

--- practice11.py
+++ practice11.py
@@ -37,7 +37,6 @@ print(m1.rating)
 # 5.) Create a class Animal with method sound() that prints "Animal makes sound".
 
 class Animal:
-    pass
     def sound(self):
         print("Animal Makes Sound!")
 
@@ -47,7 +46,6 @@ a1.sound()
 # 6.) Create a class Phone with method call() that prints "Calling...".
 
 class Phone():
-    pass
     def call(self):
         return "Calling.....!"
 
@@ -57,7 +55,6 @@ print(p1.call())
 # 7.) Create a class Laptop with method start() that prints "Laptop started".
 
 class Laptop:
-    pass
     def start(self):
         print("Laptop Started!")
 
@@ -80,7 +77,6 @@ print(s2.name)
 # 9.) Create a class Car and create 3 objects, call a method using all.
 
 class Car:
-    pass
     def type(self,name):
         self.name = name
 
@@ -108,66 +104,116 @@ p1.greet()
 
 # Level 2 – Using Object Attributes (11–20):
 
-# Create a class Student and set name using object like:
+# 11.) Create a class Student and set name using object like:
     # s1.name = "Ram"
     # Print it.
 
+class Student:
+    name = ""
+       
+s1 = Student()
+s1.name = "Ram"
+print(s1.name)
+
+# 12.) Create 2 student objects and assign different names to both. Print both.
+
+class Student:
+    def std_info(self, name, section):
+        self.name = name
+        self.section = section
+
+
+s1 = Student()
+s1.std_info("Ram Ghale", 'B')
+
+s2 = Student()
+s2.std_info("Bikash Thakur", 'D')
+
+print(s1.name)
+print(s2.name)
+
+
+# 13.) Create a class Car, assign color to different objects, and print.
+
+class Car:
+    def car_color(self,color):
+        self.Color = color
+
+c1 = Car()
+c1.car_color("Red Car")
+print(c1.Color)
+
+c2 = Car()
+c2.car_color("Black Car")
+print(c2.Color)
+
+# 14.) Create a class Book, assign price to 2 objects and print both.
+
+class Book:
+    def book_prices(self,name,price):
+        self.Name = name
+        self.Price = price
+
+b1 = Book()
+b1.book_prices("Bhagvad Geeta", 450)
+print(f"Book Name : {b1.Name}, Price : {b1.Price}")
+
+b2 = Book()
+b2.book_prices("Python Programming-2025", 425)
+print(f"Book Name : {b2.Name}, Price : {b2.Price}")
+
+
+# 15.) Create a class Movie, assign name and rating to an object and print both.
+
+class Movie:
+    def mov_rating(self,name,rating):
+        self.Name = name
+        self.Rating = rating
+
+m1 = Movie()
+m1.mov_rating("Lion King",9.7)
+print(m1.Name)
+print(m1.Rating)
+
+
+# 16.) Create 2 objects of class Phone and assign different brands.
+
+class Phone:
+    def phone_brand(self,brand):
+        self.Brand = brand
+
+p1 = Phone()
+p1.phone_brand("Samsung Galaxy A17 5G")
+print(p1.Brand)
+
+p2 = Phone()
+p2.phone_brand("Iphone 17")
+print(p2.Brand)
+
+
+# 17.) Create a class Student, assign name and age to object and print.
+
+class Student:
+    def std_info(self,name,age):
+        self.Name = name
+        self.Age = age
+        
+s1 = Student()
+s1.std_info("Shyam Ghale", 19)
+print(s1.Name)
+print(s1.Age)
+
+
+# 18.) Create a class Animal, assign type to 2 objects and print.
 
 
 
 
-
-# Create 2 student objects and assign different names to both. Print both.
-
+# 19.) Create a class Laptop, assign brand and price to object.
 
 
 
-
-
-# Create a class Car, assign color to different objects, and print.
-
-
-
-
-
-
-
-# Create a class Book, assign price to 2 objects and print both.
-
-
-
-
-
-
-# Create a class Movie, assign name and rating to an object and print both.
-
-
-
-
-
-
-# Create 2 objects of class Phone and assign different brands.
-
-
-
-
-
-# Create a class Student, assign name and age to object and print.
-
-
-
-
-
-# Create a class Animal, assign type to 2 objects and print.
-
-
-
-
-# Create a class Laptop, assign brand and price to object.
-
-
-
-# Create 3 objects of class Car, assign different colors to each.
+# 20.) Create 3 objects of class Car, assign different colors to each.
 
 
 
