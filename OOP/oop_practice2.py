d08c3aa76c9cb270461e44b63653685120167fff
Zeeson Manandhar <zeeson.manandhar11@gmail.com>
2026-04-01 21:19:28 +0545

    Solved some Questions

--- OOP/oop_practice2.py
+++ OOP/oop_practice2.py
@@ -4,51 +4,124 @@
 
 # 1.) Write a program to create a class Student with a private variable __name and a getter method to print it.
 
+class Student:
+    __name = "Zeeson Manandhar"
+    def get_name(self):
+        return self.__name
+
+s1 = Student()
+print(s1.get_name())
 
 # 2.) Write a program to create a class Person with a private variable __age and a method to display age.
 
+class Person:
+    __age = 22
+    def get_age(self):
+        return self.__age
 
+p1 = Person()
+print(p1.get_age())
 
 # 3.) Write a program to create a class Car with private variable __brand and print it using a getter.
 
+class Car:
+    __brand = "Toyota"
+    def get_brand(self):
+        print(self.__brand)
 
+c1 = Car()
+c1.get_brand()
 
 # 4.)  Write a program to create a class Book with private variable __title and access it using a method.
 
+class Book:
+    __title = "Wonder"
+    def get_title(self):
+        print(self.__title)
 
+b1 = Book()
+b1.get_title()
 
 # 5.) Write a program to create a class User with private variable __username and print it safely.
 
-
+class User:
+    __username = "ZeesonGroot"
+    def get_username(self):
+        return self.__username
+usr = User()
+print(usr.get_username())
 
 # 6.) Write a program to create a class Mobile with private variable __price and return it using a method.
 
-
+class Mobile:
+    __price = 50000
+    def get_price(self):
+        return self.__price
+m1 = Mobile()
+print(m1.get_price())
 
 # 7.) Write a program to create a class Laptop with private variable __model and display it.
 
+class Laptop:
+    __model = "Dell 3511"
+    def get_model(self):
+        print(self.__model)
 
+l1 = Laptop()
+l1.get_model()
 
 # 8.) Write a program to create a class City with private variable __name and print it using a getter.
 
+class City:
+    __name = "Kathmandu"
+    def get_city_name(self):
+        print(self.__name)
+
+c1 = City()
+c1.get_city_name()
 
 
 # 9.) Write a program to create a class Animal with private variable __type and display it.
 
-
-
+class Animal:
+    __type = "Wild Animal"
+    def get_type(self):
+        print(self.__type)
+        
+a1 = Animal()
+a1.get_type()
+    
 
 # 10.) Write a program to create a class Movie with private variable __rating and print it.
 
+class Movie:
+    __rating = 8
+    def get_rating(self):
+        print(self.__rating)
+
+m1 = Movie()
+m1.get_rating()
 
 
 # 11.) Write a program to create a class Employee with private variable __salary and show it using a method.
 
-
+class Employee:
+    __salary = 18000
+    def get_salary_info(self):
+        print(self.__salary)
+        
+e1 = Employee()
+e1.get_salary_info()
 
 # 12.) Write a program to create a class Bank with private variable __balance and print it.
 
+class Bank:
+    __balance = 7000
+    def check_balance(self):
+        print(self.__balance)
 
+b1 = Bank()
+b1.check_balance()
 
 # 13.) Write a program to create a class Teacher with private variable __subject and display it.
 
@@ -65,11 +138,32 @@
 
 
 #  INTERMEDIATE LEVEL (16–30):
+
 # Write a program to create a class Student with private variable __marks and use getter and setter methods.
+
+
+
 # Write a program to create a class Person with private variable __age and validate age > 0 in setter.
+
+
+
+
 # Write a program to create a class Bank with private variable __balance and update it using setter only if amount ≥ 0.
+
+
+
+
 # Write a program to create a class Product with private variable __price and prevent negative price.
+
+
+
+
 # Write a program to create a class Account with private variable __balance and allow deposit only if amount > 0.
+
+
+
+
+
 # Write a program to create a class Account with private variable __balance and allow withdrawal only if balance is sufficient.
 # Write a program to create a class Employee with private variable __salary and increase salary using setter.
 # Write a program to create a class Temperature with private variable __celsius and validate it is not below absolute zero.
