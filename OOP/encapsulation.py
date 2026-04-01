a901b69f541eadb010c67ef1d53416f85026e5e3
Zeeson Manandhar <zeeson.manandhar11@gmail.com>
2026-04-01 17:13:26 +0545

    updated

--- OOP/encapsulation.py
+++ OOP/encapsulation.py
@@ -89,7 +89,7 @@ b1.balance = -5000  # not allowed
 
 # Ways to make data private in Python:
 
-# a.) Single Underscore (_variable):
+# a.) Single Underscore (_variable) / Protected Variable:
 
 # For Example:
 
@@ -109,7 +109,7 @@ class Student:
 s1 = Student()
 print(s1._age)   # still works 
 
-# b.) Double Underscore (__variable):
+# b.) Double Underscore (__variable) / Private Variable:
 
 # For Example:
 
@@ -183,11 +183,167 @@ s1.getAge()
 # Simply, Getter = read private data safely
 
 # 6.) Setter Methods
+# A setter is a method used to change (set) private data safely.
 
-# Without using:
+# Without using setter:
 class Amount:
     balance = 5000
     
 a1 = Amount()
 a1.balance = -2000
 print(a1.balance)
+
+# - No Control
+# - Invalid Values Allowed
+
+# Example using setter:
+
+class Student:
+    __age = 14        # private variable
+    
+    def std_info(self,new_age):    # setter method
+        self.__age = new_age       # Updating values safely
+            
+s1 = Student()
+s1.std_info(15)
+
+# but here, in the above code: there is still a problem
+s1.std_info(-5)          # Age cannot be in neagtive value.
+
+# It is still allowing negative values.
+
+# Example using Setter with proper Validation:
+
+class Student:
+    __age = 14
+    
+    def set_info(self,new_age):
+        if new_age > 0:
+            self.__age = new_age
+            print(self.__age)
+        else:
+            print("Invalid Age!")
+            
+s1 = Student()
+s1.set_info(10)
+
+# 7.) Full Encapsulation Example
+
+# Real-life Example: Bank Account
+    # - Hide balance
+    # - Read it using getter
+    # - Update it using setter with rules
+    
+# Simple Code Example:
+
+class Bank:
+    __balance = 5000                # private data 
+    
+    def get_balance(self):         # getter method
+        print(self.__balance)
+        
+    def add_balance(self,amount):     # setter method
+        if amount > 0:                # validation
+            print(f"Your New Balance is {self.__balance + amount}")
+        else:
+            print("Invalid Amount")       # prevents wrong value
+            
+b1 = Bank()
+b1.get_balance()
+b1.add_balance(5000)
+
+# In Short, This shows:
+    # Data is hidden 
+    # Access is controlled 
+    # Wrong values blocked   
+
+
+# 8.) Data Hiding Concept:
+
+# --> Data hiding means keeping important data hidden so it cannot be accessed or changed directly.
+# --> Not everyone should access everything.
+
+# Real-life examples:
+
+# a.) Bank
+# Balance should be hidden.
+# Withdraw system should be allowed.
+
+# b.) Login System
+# Password should be hidden.
+# Login Function is allowed.
+
+
+# Why data hiding is important
+
+    # a.) Security
+    # Prevents misuse
+    # Example:
+    # No one can set balance = -10000
+
+    # b.) Control 
+    # Only allowed operations
+    # Example:
+    # Withdraw only if enough balance
+
+    # c.) Prevent mistakes 
+    # Avoid wrong data
+    # Example:
+    # Age cannot be negative
+
+    # d.) Clean design 
+    # User only sees what is needed
+
+
+# Without data hiding:
+
+b1.balance = -99999
+
+# This breaks the System.
+
+# With Data Hiding:
+b1.set_balance(-99999)   # Blocked
+
+
+# 9.) Naming Convention in Python
+
+# Python represents public, protected, and private data using naming convention.
+
+# A.) Public Variable:
+
+name = "Zeeson Manandhar"
+
+# - No underscore
+# - Fully open
+# Accessing and Modifiying data are allowed here.
+
+# B.) Protected Variable (_variable):
+
+_age = 55
+
+# Single underscore(_) used
+
+# Meaning:
+# “This is internal. Please don’t access it directly.”
+
+# Still accessible But not recommended
+
+# C.) Private Variable(__variable):
+
+__age = 50
+
+# Double underscore (__) used
+
+# Meaning:
+# “This is private. Do not access directly.”
+
+# Direct access gives error and can be accessed via methods
+
+# Simple Example:
+
+# class Student:
+#     __age = 20
+
+# s1 = Student()
+# print(s1.__age)   # throws error
+
