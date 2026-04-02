02a0e3f55fa4fd11fcee6b3713e8046eb9c9c13b
Zeeson Manandhar <zeeson.manandhar11@gmail.com>
2026-04-02 15:43:25 +0545

    solved some questions

--- OOP/oop_practice2.py
+++ OOP/oop_practice2.py
@@ -156,41 +156,167 @@ c1.get_capital()
 
 #  INTERMEDIATE LEVEL (16–30):
 
-# Write a program to create a class Student with private variable __marks and use getter and setter methods.
+# 16.) Write a program to create a class Student with private variable __marks and use getter and setter methods.
+
+class Student:
+    __marks = 0
+    def get_marks(self):
+        return self.__marks
+    
+    def update_marks(self,new_marks):
+        if new_marks >= 0 and new_marks <=100:
+            self.__marks = new_marks
+
+s1 = Student()
+s1.update_marks(55)
+print(s1.get_marks())
+
+
+# 17.) Write a program to create a class Person with private variable __age and validate age > 0 in setter.
+
+class Person:
+    __age = 25
+    def set_age(self,new_age):
+        if new_age > 0:
+            self.__age = new_age
+        else:
+            print("Not a Valid Age!")
+            
+    def get_age(self):
+        return self.__age
+
+p1 = Person()
+p1.set_age(35)
+print(p1.get_age())
+
+
+# 18.) Write a program to create a class Bank with private variable __balance and update it using setter only if amount ≥ 0.
+
+class Bank:
+    __balance = 1000
+    def set_balance(self,new_balance):
+        self.__balance = self.__balance + new_balance
+       
+    def check_balance(self):
+        return self.__balance
+    
+b1 = Bank()
+b1.set_balance(5500)
+print(b1.check_balance())
+
+
+# 19.) Write a program to create a class Product with private variable __price and prevent negative price.
+
+class Product:
+    __price = 0
+    
+    def set_price(self,new_price):
+        if new_price > 0:
+            self.__price = new_price
+        else:
+            print("Negative Price not accepted!")
+
+    def get_price(self):
+        return self.__price
+    
+p1 = Product()
+p1.set_price(4500)
+print(p1.get_price())
+
+
+# 20.) Write a program to create a class Account with private variable __balance and allow deposit only if amount > 0.
+
+class Account:
+    __balance = 0
+    def set_balance(self,new_balance):
+        if new_balance > 0:
+            self.__balance = self.__balance + new_balance
+        else:
+            print("Invalid Amount!")
+    
+    def get_balance(self):
+        return self.__balance
+    
+a1 = Account()
+a1.set_balance(6745)
+print(a1.get_balance())
+
+
+# 21.) Write a program to create a class Account with private variable __balance and allow withdrawal only if balance is sufficient.
+
+class Account:
+    __balance = 1000   
+
+    def get_balance(self):
+        return self.__balance
+
+    def withdraw(self, amount):
+        if amount > 0 and amount <= self.__balance:
+            self.__balance -= amount
+            print(f"{amount} withdrawn successfully!")
+        else:
+            print("Insufficient balance!")
+
+
+a1 = Account()
+# Check balance
+print("Initial Balance:", a1.get_balance())
+
+# Withdraw money
+a1.withdraw(600)
+print("Remaining Balance:", a1.get_balance())
+
+
+# 22.) Write a program to create a class Employee with private variable __salary and increase salary using setter.
+
+class Employee:
+    __salary = 15000
+    def increase_salary(self,amount):
+        self.__salary += amount
+    def check_salary(self):
+        return self.__salary
+
+e1 = Employee()
+print("Previous Salary:",e1.check_salary())
+
+e1.increase_salary(3000)
+print("Latest Salary:",e1.check_salary())
+
+
+# 23.) Write a program to create a class Temperature with private variable __celsius and validate it is not below absolute zero.
+
+
+# 24.) Write a program to create a class User with private variable __password and method to check password.
 
 
 
-# Write a program to create a class Person with private variable __age and validate age > 0 in setter.
+# 25.) Write a program to create a class Exam with private variable __marks and ensure marks are between 0 and 100.
+
+
+
+# 26.) Write a program to create a class Car with private variable __speed and restrict speed limit.
+
+
+
+# 27.) Write a program to create a class Wallet with private variable __money and methods to add and spend money.
+
+
+
+# 28.) Write a program to create a class Laptop with private variable __battery and prevent battery level > 100.
 
 
 
 
-# Write a program to create a class Bank with private variable __balance and update it using setter only if amount ≥ 0.
+# 29.) Write a program to create a class Library with private variable __books and return count using getter.
 
 
 
 
-# Write a program to create a class Product with private variable __price and prevent negative price.
+# 30.) Write a program to create a class Game with private variable __score and update score using setter.
 
 
 
 
-# Write a program to create a class Account with private variable __balance and allow deposit only if amount > 0.
-
-
-
-
-
-# Write a program to create a class Account with private variable __balance and allow withdrawal only if balance is sufficient.
-# Write a program to create a class Employee with private variable __salary and increase salary using setter.
-# Write a program to create a class Temperature with private variable __celsius and validate it is not below absolute zero.
-# Write a program to create a class User with private variable __password and method to check password.
-# Write a program to create a class Exam with private variable __marks and ensure marks are between 0 and 100.
-# Write a program to create a class Car with private variable __speed and restrict speed limit.
-# Write a program to create a class Wallet with private variable __money and methods to add and spend money.
-# Write a program to create a class Laptop with private variable __battery and prevent battery level > 100.
-# Write a program to create a class Library with private variable __books and return count using getter.
-# Write a program to create a class Game with private variable __score and update score using setter.
 # ADVANCED LEVEL (31–40):
 # Write a program to create a class Bank with private variable __balance, and methods for deposit, withdraw, and check balance with full validation.
 # Write a program to create a class ATM with private method __authenticate() and public method login().
