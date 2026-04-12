0441342c22db32c4da2e94f6e7dfc166f8c4709e
Zeeson Manandhar <zeeson.manandhar11@gmail.com>
2026-04-12 11:05:50 +0545

    added Abstraction

---
+++ OOP/abstraction.py
@@ -0,0 +1,55 @@
+# Abstraction:
+
+# Abstraction simply means: Show only important things and hide unnecessary details.
+
+# In very simple words
+# - “Only what user needs to see”.
+
+# Real-Life Examples:
+
+# Example 1 – Car
+# We drive car using:
+    # steering
+    # brake
+    # accelerator
+
+# But We DON’T see:
+    # engine logic
+    # internal wiring
+
+# That is abstraction.
+
+# Example 2 – ATM
+# We insert card
+    # enter PIN
+    # withdraw money
+
+# We DON’T see:
+    # bank server
+    # transaction processing
+
+# That is abstraction
+
+
+# Programming Meaning
+# In Python:
+# - User sees method
+# - Hidden = how it is implemented
+
+# Why We Use Abstraction?
+
+# To:
+    # hide complexity
+    # show only necessary parts
+    # make code clean
+    # enforce rules
+    
+# Small Example:
+def withdraw():
+    print("Money withdrawn")
+    
+# User only sees:
+# withdraw()
+# Not how bank works internally
+
+# In Conclusion, Abstraction = hide logic + show functionality.
